b2ad76065819750fce43cfe42e15a357cbe24bbd
Jonathan Rodríguez Alejos <jonathan.rodriguez@roiback.com>
2018-10-15 10:19:37 +0200

    Eliminando espacios de sobra

--- react-context-api/src/App.js
+++ react-context-api/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+
 import TodoList from './TodoList';
 import TodoItem from './TodoItem';
 
 import logo from './logo.svg';
 import './App.css';
-
 
 
 class App extends React.Component {
